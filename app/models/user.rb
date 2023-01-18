7574677935611cd43666ca467efd59c3781d7eb1
OmarMWarraich <earthling986@gmail.com>
2023-01-19 00:39:32 +0500

    feat: Add Custom Methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
-    has_many :posts
-    has_many :comments
-    has_many :likes
+  has_many :posts
+  has_many :comments
+  has_many :likes
+
+  def recent_three_posts
+    posts.order(created_at: :desc).limit(3)
+  end
 end
