a3f64c63f46aa41dfd4b17587d6d4bd3d853a9e9
OmarMWarraich <earthling986@gmail.com>
2023-01-19 19:17:21 +0500

    feat: Add Validations to Models

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,10 @@ class Post < ApplicationRecord
   has_many :comments
   has_many :likes
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :comments_counter, numericality: { integer_only: true, greater_than_or_equal_to: 0 }
+  validates :likes_counter, numericality: { integer_only: true, greater_than_or_equal_to: 0 }
+
   after_save :update_posts_counter
 
   def recent_five_comments
