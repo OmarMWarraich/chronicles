f16c84954dfca103b098afcea7a8400dc9da0942
OmarMWarraich <earthling986@gmail.com>
2023-01-19 20:25:12 +0500

    feat: Add Unit Tests for Remaining Models

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,30 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    let (:user) { User.create!(name: 'John', posts_counter: 0) }
-    let (:post) { Post.create!(title: 'Post title', text: 'Post text', author: user, comments_counter: 0, likes_counter: 0 ) }
+  let(:user) { User.create!(name: 'John', posts_counter: 0) }
+  let(:post) do
+    Post.create!(title: 'Post title', text: 'Post text', author: user, comments_counter: 0, likes_counter: 0)
+  end
 
-    it 'should have many posts' do
-        expect(user.posts).to eq([post])
-    end
+  it 'should have many posts' do
+    expect(user.posts).to eq([post])
+  end
 
-    it 'should have many comments' do
-        expect(user.comments).to eq([])
-    end
+  it 'should have many comments' do
+    expect(user.comments).to eq([])
+  end
 
-    it 'should have many likes' do
-        expect(user.likes).to eq([])
-    end
+  it 'should have many likes' do
+    expect(user.likes).to eq([])
+  end
 
-    it 'should have a name' do
-        expect(user.name).to eq('John')
-    end
+  it 'should have a name' do
+    expect(user.name).to eq('John')
+  end
 
-    it 'should have a posts_counter' do
-        expect(user.posts_counter).to eq(0)
-    end
+  it 'should have a posts_counter' do
+    expect(user.posts_counter).to eq(0)
+  end
 
-    it 'should have a recent_three_posts' do
-        expect(user.recent_three_posts).to eq([post])
-    end
+  it 'should have a recent_three_posts' do
+    expect(user.recent_three_posts).to eq([post])
+  end
 end
