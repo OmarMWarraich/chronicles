99bda9979740ad1c29bf8b8341e94dd829165575
OmarMWarraich <earthling986@gmail.com>
2023-01-19 18:44:35 +0500

    fix: Change dtype of Likes_Counter to bigint

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_18_185307) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_19_134253) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -37,10 +37,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_18_185307) do
     t.string "title"
     t.string "text"
     t.bigint "comments_counter"
-    t.string "likes_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
+    t.bigint "likes_counter", default: 0
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
