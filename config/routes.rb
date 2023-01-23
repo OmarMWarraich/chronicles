bfd6a1398665fa4d193c207df7c7db783c81b0e4
OmarMWarraich <earthling986@gmail.com>
2023-01-24 02:10:17 +0500

    feat: Set Root as Users && Path as users/:id for show
    feat: Add Params in users_controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,10 @@ Rails.application.routes.draw do
   get 'posts/index'
   get 'posts/show'
   get 'users/index'
-  get 'users/show'
+  get '/users/:id', to: 'users#show', as: 'user'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "users#index"
 end
