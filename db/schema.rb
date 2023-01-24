84c3f5db697de4524d663924fd9d8eced20a4b7c
OmarMWarraich <earthling986@gmail.com>
2023-01-24 10:41:34 +0500

    fix: Rectify Counter's Default Value to 0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_19_135600) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_24_053049) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -48,9 +48,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_19_135600) do
     t.string "name"
     t.text "photo"
     t.string "bio"
-    t.bigint "posts_counter"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "posts_counter", default: 0, null: false
   end
 
   add_foreign_key "comments", "posts"
