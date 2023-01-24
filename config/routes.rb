1988f515795194be28da81e690905f9aafd567a1
OmarMWarraich <earthling986@gmail.com>
2023-01-24 20:49:54 +0500

    fix: Revert Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in   
-  get 'users/index'
-  root 'users#show'
+  get 'users/:user_id/posts/', to: 'posts#index', as: 'user_posts'
+  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
+  get '/users/:id', to: 'users#show', as: 'user'
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
+  root "users#index", as: 'users'
 end
