86e4842f1325ccd06471f49a4b97bf7222e83738
OmarMWarraich <earthling986@gmail.com>
2023-01-24 20:18:07 +0500

    fix: Rectify User views as per reg

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/:user_id/posts/', to: 'posts#index', as: 'user_posts'
-  get 'users/:user_id/posts/:id', to: 'posts#show', as: 'user_post'
-  get '/users/:id', to: 'users#show', as: 'user'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  # Define your application routes per the DSL in   
+  get 'users/index'
+  root 'users#show'
   # Defines the root path route ("/")
-  root "users#index", as: 'users'
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :show]
+  end
 end
