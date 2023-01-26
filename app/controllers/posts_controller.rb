801f19e080ed93f51eb93095f14a148ac630753e
OmarMWarraich <earthling986@gmail.com>
2023-01-26 19:58:34 +0500

    style: Add Design to the Post(s) and User(s)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,6 @@ class PostsController < ApplicationController
   def show
     @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
+    @comments = Comment.where(post_id: params[:id])
   end
 end
