a0ac92a506861b23c3ee4c78db564cdef006526d
OmarMWarraich <earthling986@gmail.com>
2023-01-28 13:19:40 +0500

    feat: Add Partial Forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,10 @@ Rails.application.routes.draw do
   
   resources :users, only: [:index, :show] do  
     resources :posts, only: [:index, :new, :create, :show] do
-      resources :comments, only: [:new, :create] do
+      resources :comments, only: [:new, :create] 
       resources :likes, only: [:create]
     end
   end
-end
 
   root "users#index"
 end
