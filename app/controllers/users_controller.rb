421358ad2f92ef96694717b3b1f32cc3f7699149
OmarMWarraich <earthling986@gmail.com>
2023-01-29 12:33:56 +0500

    fix: Update Links and README

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,5 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @posts = Post.where(author_id: params[:id])
-    @post = Post.find(params[:id])
   end
 end
