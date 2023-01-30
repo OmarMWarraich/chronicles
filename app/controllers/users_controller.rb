53fc9f1575a1dcd3abaf87e399e1fbd5b061c9e8
OmarMWarraich <earthling986@gmail.com>
2023-01-31 02:15:51 +0500

    refactor: User's Show Page w/ style

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @users = User.all
     @user = User.find(params[:id])
-    @posts = Post.where(author_id: params[:id])
+    @posts = @user.posts.order(id: :asc).limit(3)
   end
 end
