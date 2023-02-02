f4944f40da92803e556bf075223f0c304121420e
Zuhaib Amjad <umar0423@gmail.com>
2023-02-02 14:56:15 +0500

    Add specs to users_show view file

--- spec/features/users_show_spec.rb
+++ spec/features/users_show_spec.rb
@@ -1,1 +1,16 @@
 require 'rails_helper'
+RSpec.describe 'UsersShow', type: :system do
+  let(:user) { FactoryBot.create(:user) }
+  let(:other_user) { FactoryBot.create(:user) }
+  before do
+    driven_by(:rack_test)
+  end
+  it 'shows the user profile' do
+    visit user_path(user)
+    expect(page).to have_content(user.name)
+  end
+  it 'shows the user profile' do
+    visit user_path(other_user)
+    expect(page).to have_content(other_user.name)
+  end
+end
