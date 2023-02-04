f36c9df71622e9ee878fca4fe5bb0be9fc0dc749
OmarMWarraich <earthling986@gmail.com>
2023-02-04 14:49:26 +0500

    feat: Auth Rules AppController

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-
-  devise_scope :user do
-    get "/", to: "devise/sessions#new"
-  end
-
   
   resources :users, only: [:index, :show] do  
     resources :posts, only: [:index, :new, :create, :show] do
