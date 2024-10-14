40aa509efb18411ed0e17833c7f82195009f58f7
OmarMWarraich <earthling986@gmail.com>
2024-10-15 04:30:05 +0500

    feat: Reset Instructions Mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,15 +37,27 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Set up mailer for development.
-  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    address: "smtp.mandrillapp.com",
+    port: 587,
+    domain: "topgeardirect.com",
+    user_name: "omar@topgeardirect.com",
+    password: "md-vysX-FPutKC4inTI764QWg",
+    authentication: "login",
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default charset: "utf-8"
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
 
   # Set up default URL options for the Devise mailer in each environment.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
